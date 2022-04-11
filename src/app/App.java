a651543dd2975cada8ec5e4f18add3e1a770f1f9
Dezimund <de3mond15@gmail.com>
2022-04-11 21:57:12 +0300

    Add Input via BufferReader

--- src/app/App.java
+++ src/app/App.java
@@ -1,8 +1,13 @@
 package app;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 // Класс-входная точка в приложение.
 // App launcher.
 public class App {
+
+    static final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
 
     static String merchantName;
     static String email;
@@ -17,20 +22,26 @@ public class App {
     static String infoProduct;
 
     // Делаем метод main() наименее загруженным логикой
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         initVars();
         showData(processData());
     }
 
     // Инициализация переменных.
     // Имитация ввода данных пользователем.
-    private static void initVars() {
-        merchantName = "Роберт";
-        email = "bob@mail.com";
-        phone = "055 123-4567";
-        productName = "апельсины";
-        quantity = 1000;
-        price = 19.99;
+    private static void initVars() throws IOException {
+        System.out.println("Input Name");
+        merchantName = READER.readLine();
+        System.out.println("Input email");
+        email = READER.readLine();
+        System.out.println("Input phone");
+        phone = READER.readLine();
+        System.out.println("Input Product Name");
+        productName = READER.readLine();
+        System.out.println("Input quantity");
+        quantity = Integer.parseInt(READER.readLine());
+        System.out.println("Input price");
+        price = Double.parseDouble(READER.readLine());
     }
 
     // Передача данных на обработку.
